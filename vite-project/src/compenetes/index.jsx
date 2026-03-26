d9dff660778abfa811e7098ff9b9b467442473c5
andré <aluno.den@senaibahia.com.br>
2026-03-26 16:54:28 -0300

    salvar

---
+++ vite-project/src/compenetes/index.jsx
@@ -0,0 +1,9 @@
+import './style.css'
+
+function Mensagem() {
+    return (
+         // Comandos HTML.
+     class
+    <h1><Componente></Componente>!</h1>
+        <p>Este é um componente com uma mensaqgem simples.</p>
+}
